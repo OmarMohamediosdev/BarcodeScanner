3707f516d20f68fa846982b6e2d85ba3225132d7
OmarMohamediosdev <45975191+OmarMohamediosdev@users.noreply.github.com>
2024-08-18 13:36:41 +0300

    Add files via upload

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Omar Mohamed on 18/08/2024.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+        
+}
