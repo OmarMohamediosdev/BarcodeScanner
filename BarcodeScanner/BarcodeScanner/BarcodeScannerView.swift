11a07c16cc75b38c2d0da4e3791595721100f481
Omar Mohamed <omar.mohamed.iosdev@gmail.com>
2024-08-19 02:21:23 +0300

    remove not working code

--- BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import VisionKit
 
 struct BarcodeScannerView: View {
+    @State var isShowingScanner = true
+    @State private var scannedText = ""
+    
     var body: some View {
         VStack {
             NavigationStack {
@@ -15,7 +19,25 @@ struct BarcodeScannerView: View {
 //                    Rectangle()
 //                        .frame(width: .infinity, height: 300)
                     
-                    BarcodeScanner()
+                    
+                    if DataScannerViewController.isSupported && DataScannerViewController.isAvailable {
+                        ZStack(alignment: .bottom) {
+                            DataScannerRepresentable(
+                                shouldStartScanning: $isShowingScanner,
+                                scannedText: $scannedText,
+                                dataToScanFor: [.barcode()]
+                            )
+                            
+                            Text(scannedText)
+                                .padding()
+                                .background(Color.white)
+                                .foregroundColor(.black)
+                        }
+                    } else if !DataScannerViewController.isSupported {
+                        Text("It looks like this device doesn't support the DataScannerViewController")
+                    } else {
+                        Text("It appears your camera may not be available")
+                    }
                     
                     Spacer()
                         .frame(height: 60)
