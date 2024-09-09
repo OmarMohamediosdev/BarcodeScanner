96630bd96350600d60de10b2d1bf42f05e80e449
OmarMohamediosdev <45975191+OmarMohamediosdev@users.noreply.github.com>
2024-09-09 16:18:45 +0300

    stop capture session

--- BarcodeScanner/BarcodeScanner/ScannerVC.swift
+++ BarcodeScanner/BarcodeScanner/ScannerVC.swift
@@ -102,7 +102,8 @@ extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
             scannerDelegate?.didSurface(error: .invalidScannedValue)
             return
         }
-        
+            
+        captureSession.stopRunning()
         scannerDelegate?.didFind(barcode: barcode)
     }
 }
